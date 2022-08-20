17112ed3c6bfb430be4dccffb96e2fd4a3de86d4
forkyspoon1 <david.guan@sjsu.edu>
2022-08-20 08:19:32 -0700

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function App () {
+
+return (
+    <>
+    <header>
+        <Navbar />
+    </header>
+    
+    <h1>testing </h1>
+    </>
+    )
+}
+
+export default App;
